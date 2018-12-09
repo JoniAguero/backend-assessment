302635ccb47cf0a447045c57f8b4f39fd8c6a765
Joni Agüero <aguerojonathanivan@gmail.com>
2018-12-09 20:54:44 -0300

    Optimazacion

--- tests/clients.test.js
+++ tests/clients.test.js
@@ -1,12 +1,12 @@
 const chaiHttp = require('chai-http');
 const chai = require('chai'),
     expect = chai.expect;
+const config = require('../config');
 
 chai.use(chaiHttp);
-const URL = 'http://localhost:3000/api';
 describe('GET /clients', function () {
     it('should get status 200', (done) => {
-        chai.request(URL)
+        chai.request(config.API_URL)
             .get('/clients')
             .end(function (err, res) {
                 expect(res).to.have.status(200);
@@ -14,7 +14,7 @@ describe('GET /clients', function () {
             });
     });
     it('should get all clients', (done) => {
-        chai.request(URL)
+        chai.request(config.API_URL)
             .get('/clients')
             .end(function (err, res) {
                 expect(res.body.clients.length).greaterThan(1);
@@ -22,7 +22,7 @@ describe('GET /clients', function () {
             });
     });
     it('should get the client with id x', (done) => {
-        chai.request(URL)
+        chai.request(config.API_URL)
             .get('/clients/id/a0ece5db-cd14-4f21-812f-966633e7be86')
             .end(function (err, res) {
                 expect(res.body).to.have.property('id').to.be.equal('a0ece5db-cd14-4f21-812f-966633e7be86');
@@ -31,7 +31,7 @@ describe('GET /clients', function () {
             });
     });
     it('should get the client with name x', (done) => {
-        chai.request(URL)
+        chai.request(config.API_URL)
             .get('/clients/name/Britney')
             .end(function (err, res) {
                 expect(res.body).to.have.property('name').to.be.equal('Britney');
--- tests/policies.test.js
+++ tests/policies.test.js
@@ -1,14 +1,12 @@
 const chaiHttp = require('chai-http');
 const chai = require('chai'),
     expect = chai.expect;
+const config = require('../config');
 
 chai.use(chaiHttp);
-const URL = 'http://localhost:3000/api';
-
-
 describe('GET /policies', function () {
     it('should get status 200', (done) => {
-        chai.request(URL)
+        chai.request(config.API_URL)
             .get('/policies')
             .end(function (err, res) {
                 expect(res).to.have.status(200);
@@ -16,7 +14,7 @@ describe('GET /policies', function () {
             });
     });
     it('should get all policies', (done) => {
-        chai.request(URL)
+        chai.request(config.API_URL)
             .get('/policies')
             .end(function (err, res) {
                 expect(res.body.policies.length).greaterThan(1);
@@ -24,7 +22,7 @@ describe('GET /policies', function () {
             });
     });
     it('should get the list of policies linked to a user name', (done) => {
-        chai.request(URL)
+        chai.request(config.API_URL)
             .get('/policies/name/Manning')
             .end(function (err, res) {
                 expect(res.body).to.be.an('array');
@@ -33,7 +31,7 @@ describe('GET /policies', function () {
             });
     });
     it('should get the user linked to a policy number', (done) => {
-        chai.request(URL)
+        chai.request(config.API_URL)
             .get('/policies/id/64cceef9-3a01-49ae-a23b-3761b604800b')
             .end(function (err, res) {
                 expect(res.body).to.be.an('object');
