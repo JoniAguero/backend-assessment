805ffeb560652bf5cd60a99d08e5ef358cc89546
Joni Agüero <aguerojonathanivan@gmail.com>
2018-12-09 01:01:41 -0300

    Policies requests

--- routes/api/policies/policies.js
+++ routes/api/policies/policies.js
@@ -1,12 +1,14 @@
+var fetch = require('isomorphic-fetch');
 var request = require('request');
-var rp = require('request-promise');
 
 const API_PATH = '/api';
-const URL = 'http://www.mocky.io/v2/580891a4100000e8242b75c5';
+const urls = [
+    'http://www.mocky.io/v2/5808862710000087232b75ac',
+    'http://www.mocky.io/v2/580891a4100000e8242b75c5'
+];
 module.exports = (app) => {
-
     const options = {
-        uri: URL,
+        uri: urls[1],
         json: true // Automatically parses the JSON string in the response
     };
     app.get(`${API_PATH}/policies`, (req, res) => {
@@ -16,14 +18,45 @@ module.exports = (app) => {
             res.send(body)
         });
     });
-
-
-    rp(options)
-        .then(function (repos) {
-            console.log('User has %d repos', repos.length);
-        })
-        .catch(function (err) {
-            // API call failed...
-        });
-
+    app.get(`${API_PATH}/policies/name/:name`, (req, res) => {
+        const name = req.params.name
+        let requests = urls.map(url => fetch(url))
+        // Promise.all waits until all jobs are resolved
+        Promise.all(requests)
+            .then(responses => {
+                return responses
+            })
+            .then(responses => Promise.all(responses.map(r => r.json())))
+            .then(data => {
+                return data
+            })
+            .then(data => {   
+                const clients = data[0].clients // Posicion de clientes
+                const policies = data[1].policies // Posicion de policies
+                const client = clients.find(client => client.name === name) 
+                const response = policies.filter(policy => policy.clientId === client.id) 
+                res.send(response)
+            })
+        }
+    );
+    app.get(`${API_PATH}/policies/id/:id`, (req, res) => {
+        const id = req.params.id
+        let requests = urls.map(url => fetch(url))
+        // Promise.all waits until all jobs are resolved
+        Promise.all(requests)
+            .then(responses => {
+                return responses
+            })
+            .then(responses => Promise.all(responses.map(r => r.json())))
+            .then(data => {
+                return data
+            })
+            .then(data => {
+                const clients = data[0].clients // Posicion de clientes
+                const policies = data[1].policies // Posicion de policies
+                const policy = policies.find(policy => policy.clientId === id)
+                const response = clients.filter(client => client.id === policy.clientId)
+                res.send(response)
+            })
+    });
 }
