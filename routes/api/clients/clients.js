8c2dd76043fa83bfd0d5b17434c6d7d1b2050952
Joni Agüero <aguerojonathanivan@gmail.com>
2018-12-08 22:47:19 -0300

    Refactor

--- routes/api/clients/clients.js
+++ routes/api/clients/clients.js
@@ -3,8 +3,13 @@ var request = require('request');
 const API_PATH = '/api';
 const URL = 'http://www.mocky.io/v2/5808862710000087232b75ac';
 module.exports = (app) => {
+
+    const options = {
+        uri: URL,
+        json: true // Automatically parses the JSON string in the response
+    };
     app.get(`${API_PATH}/clients`, (req, res)=>{
-        request({url: URL, json: true}, function (error, response, body) {
+        request(options, function (error, response, body) {
             if(error) console.log('error:', error); // Print the error if one occurred and handle it
             console.log('statusCode:', response && response.statusCode);
             res.send(body)
@@ -12,7 +17,7 @@ module.exports = (app) => {
     });
     app.get(`${API_PATH}/clients/id/:id`, (req, res) => {
         const id = req.params.id;
-        request({url: URL, json: true}, function (error, response, body) {
+        request(options, function (error, response, body) {
             if (error) console.log('error:', error); // Print the error if one occurred and handle it
             console.log('statusCode:', response && response.statusCode);
             const client = body.clients.find(client => client.id === id);
@@ -21,7 +26,7 @@ module.exports = (app) => {
     });
     app.get(`${API_PATH}/clients/name/:name`, (req, res) => {
         const name = req.params.name;
-        request({url: URL, json: true}, function (error, response, body) {
+        request(options, function (error, response, body) {
             if (error) console.log('error:', error); // Print the error if one occurred and handle it
             console.log('statusCode:', response && response.statusCode);
             const client = body.clients.find(client => client.name === name);
--- routes/api/policies/policies.js
+++ routes/api/policies/policies.js
@@ -1,17 +1,29 @@
 var request = require('request');
+var rp = require('request-promise');
 
 const API_PATH = '/api';
 const URL = 'http://www.mocky.io/v2/580891a4100000e8242b75c5';
 module.exports = (app) => {
+
+    const options = {
+        uri: URL,
+        json: true // Automatically parses the JSON string in the response
+    };
     app.get(`${API_PATH}/policies`, (req, res) => {
-        request({
-            url: URL,
-            json: true
-        }, function (error, response, body) {
+        request(options, function (error, response, body) {
             if (error) console.log('error:', error); // Print the error if one occurred and handle it
             console.log('statusCode:', response && response.statusCode);
             res.send(body)
         });
     });
+
+
+    rp(options)
+        .then(function (repos) {
+            console.log('User has %d repos', repos.length);
+        })
+        .catch(function (err) {
+            // API call failed...
+        });
 
 }
