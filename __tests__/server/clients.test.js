4b1c1c14f859849ebf4a6c430aac0ca9eb5cc333
Joni Agüero <aguerojonathanivan@gmail.com>
2018-12-11 09:53:34 -0300

    Test with token

--- __tests__/server/clients.test.js
+++ __tests__/server/clients.test.js
@@ -1,62 +1,89 @@
 "use strict"
 const chaiHttp = require('chai-http')
 const chai = require('chai')
-
+const faker = require('faker');
 const expect = chai.expect
-const config = require('../../config')
-
-const app = require('../../index')
+const config = require('../config')
+const server = require('../../server/app');
 
 chai.use(chaiHttp)
-describe('GET /clients', function () {
-
-  it('athenticate', (done) => {
-    chai.request(app)
+let token;
+describe('Clients Routes', function () {
+  const user = {
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+    role: 'user'
+  };
+  const admin = {
+    username: 'test',
+    password: 'test',
+    role: 'admin'
+  };
+  before(async () => {
+    try {
+      const result = await chai
+        .request(server)
         .post('/authenticate')
-        .set('authorization', 'xxx')
-        .send({ user: 'test', password: 'test' })
-        .end(function(err, res) {
-            console.log(res.status);
-            console.log(res.body);
-            console.log(res.type);
-            expect(res).to.have.status(200)
-            expect(res).to.have.type('application/json')
-            done();
-        });
+        .send(admin);
+        expect(result.status).to.equal(200);
+      token = result.body.token;
+    } catch (error) {
+      console.log(error);
+    }
   });
-
-  it('should get status 200', (done) => {
-    chai.request(config.API_URL)
-      .get('/clients')
-      .end(function (err, res) {
-        expect(res).to.have.status(200)
-        done()
-      })
+  describe('GET /clients', () => {
+    it('should get status 200', (done) => {
+      try {
+      chai.request(config.API_URL)
+        .get('/clients')
+        .end(function (err, res) {
+          expect(res).to.have.status(200)
+          done()
+        })
+      } catch (error) {
+        console.log(error);
+      }
+    })
+    it('should get all clients', (done) => {
+      try {
+      chai.request(config.API_URL)
+        .get('/clients')
+        .end(function (err, res) {
+          expect(res.body.clients.length).greaterThan(1)
+          done()
+        })
+      } catch (error) {
+        console.log(error);
+      }
+    })
+    it('should get the client with id x', (done) => {
+      try {
+        
+        chai.request(config.API_URL)
+        .get('/clients/id/a0ece5db-cd14-4f21-812f-966633e7be86')
+        .set('Authorization', `Bearer ${token}`)
+        .end(function (err, res) {
+          expect(res.body).to.have.property('id').to.be.equal('a0ece5db-cd14-4f21-812f-966633e7be86')
+          expect(res).to.have.status(200)
+          done()
+        })
+      } catch (error) {
+        console.log(error);
+      }
+    })
+    it('should get the client with name x', (done) => {
+      try {
+      chai.request(config.API_URL)
+        .get('/clients/name/Britney')
+        .set('Authorization', `Bearer ${token}`)
+        .end(function (err, res) {
+          expect(res.body).to.have.property('name').to.be.equal('Britney')
+          expect(res).to.have.status(200)
+          done()
+        })
+      } catch (error) {
+        console.log(error);
+      }
+    })
   })
-  it('should get all clients', (done) => {
-    chai.request(config.API_URL)
-      .get('/clients')
-      .end(function (err, res) {
-        expect(res.body.clients.length).greaterThan(1)
-        done()
-      })
-  })
-  it('should get the client with id x', (done) => {
-    chai.request(config.API_URL)
-      .get('/clients/id/a0ece5db-cd14-4f21-812f-966633e7be86')
-      .end(function (err, res) {
-        expect(res.body).to.have.property('id').to.be.equal('a0ece5db-cd14-4f21-812f-966633e7be86')
-        expect(res).to.have.status(200)
-        done()
-      })
-  })
-  it('should get the client with name x', (done) => {
-    chai.request(config.API_URL)
-      .get('/clients/name/Britney')
-      .end(function (err, res) {
-        expect(res.body).to.have.property('name').to.be.equal('Britney')
-        expect(res).to.have.status(200)
-        done()
-      })
-  })
-})
+});
--- __tests__/server/policies.test.js
+++ __tests__/server/policies.test.js
@@ -2,41 +2,83 @@ const chaiHttp = require('chai-http')
 const chai = require('chai')
 
 const expect = chai.expect
-const config = require('../../config')
+const config = require('../config')
+const server = require('../../server/app');
 
 chai.use(chaiHttp)
-describe('GET /policies', function () {
-  it('should get status 200', (done) => {
-    chai.request(config.API_URL)
-      .get('/policies')
-      .end(function (err, res) {
-        expect(res).to.have.status(200)
-        done()
-      })
+let token;
+describe('Policies Routes', function () {
+  const admin = {
+    username: 'test',
+    password: 'test',
+    role: 'admin'
+  };
+  before(async () => {
+    try {
+      const result = await chai
+        .request(server)
+        .post('/authenticate')
+        .send(admin);
+      expect(result.status).to.equal(200);
+      token = result.body.token;
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  describe('GET /policies', function () {
+    it('should get status 200', (done) => {
+      try {
+      chai.request(config.API_URL)
+        .get('/policies')
+        .end(function (err, res) {
+          expect(res).to.have.status(200)
+          done()
+        })
+      } catch (error) {
+        console.log(error);
+      }
+    })
+    it('should get all policies', (done) => {
+      try {
+      chai.request(config.API_URL)
+        .get('/policies')
+        .end(function (err, res) {
+          expect(res.body.policies.length).greaterThan(1)
+          done()
+        })
+      } catch (error) {
+        console.log(error);
+      }
+    })
+    xit('should get the list of policies linked to a user name', (done) => {
+      try {
+        chai.request(config.API_URL)
+        .get('/policies/name/Manning')
+        .set('Authorization', `Bearer ${token}`)
+        .set('Credentials', `${admin.role}`)
+        .end(function (err, res) {
+          expect(res.body).to.be.an('array')
+          expect(res.body.length).greaterThan(1)
+          done()
+        })
+      } catch (error) {
+        console.log(error);
+      }
+    })
+    xit('should get the user linked to a policy number', (done) => {
+      try {
+        chai.request(config.API_URL)
+        .get('/policies/id/64cceef9-3a01-49ae-a23b-3761b604800b')
+        .set('Authorization', `Bearer ${token}`)
+        .set('Credentials', `${admin.role}`)
+        .end(function (err, res) {
+          expect(res.body).to.be.an('object')
+          done()
+        })
+      } catch (error) {
+        console.log(error);
+      }
+    })
   })
-  it('should get all policies', (done) => {
-    chai.request(config.API_URL)
-      .get('/policies')
-      .end(function (err, res) {
-        expect(res.body.policies.length).greaterThan(1)
-        done()
-      })
-  })
-  it('should get the list of policies linked to a user name', (done) => {
-    chai.request(config.API_URL)
-      .get('/policies/name/Manning')
-      .end(function (err, res) {
-        expect(res.body).to.be.an('array')
-        expect(res.body.length).greaterThan(1)
-        done()
-      })
-  })
-  it('should get the user linked to a policy number', (done) => {
-    chai.request(config.API_URL)
-      .get('/policies/id/64cceef9-3a01-49ae-a23b-3761b604800b')
-      .end(function (err, res) {
-        expect(res.body).to.be.an('object')
-        done()
-      })
-  })
-})
+});
+
